970e547d194e57f69e15091e0c56753681ba2275
Shaganashree P L <shaganashree.pl@gmail.com>
2025-02-11 13:45:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div style={{ textAlign: "center", padding: "50px" }}>
+            <h1>Welcome to Story Collaboration Platform</h1>
+            <p>Start creating and sharing stories with others in real-time!</p>
+        </div>
+    );
+};
+
+export default App;
